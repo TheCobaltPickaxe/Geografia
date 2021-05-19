9d074e00e545ee8ebf039861238c5e9adbc28da8
Pedor <thecobaltpickaxe@gmail.com>
2021-05-19 19:48:48 -0300

    Whatsapp support

--- code.js
+++ code.js
@@ -3,13 +3,25 @@ window.onload = function(){
     form.addEventListener("submit", (e) => {
         e.preventDefault()
         const data = Object.fromEntries(new FormData(form).entries())
-        console.log(data)
         var desc = "Grupo: " + data.name + "\nPedra: " + data.rock + "\n\n" + data.message
         var file = new Blob([desc], {type: "text/plaintext"})
+
         const a = document.createElement('a');
         a.href = URL.createObjectURL(file), "_blank"
         a.download = data.rock + ".txt"
         a.click()
         a.remove()
     }, false)
+
+    var whatsappBtn = document.getElementById("whatsapp")
+    whatsappBtn.addEventListener("click", (e) =>{
+        const data = Object.fromEntries(new FormData(form).entries())
+        var desc = "Grupo: " + data.name + "\nPedra: " + data.rock + "\n\n" + data.message
+        var file = new Blob([desc], {type: "text/plaintext"})
+
+        const a = document.createElement('a')
+        a.href = "https://wa.me?text=" + encodeURIComponent(desc)
+        a.click()
+        a.remove()
+    })
 }
