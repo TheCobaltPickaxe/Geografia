cf33e389bb5c5995b97eb11c9e530d5248dfc76e
Pedor <thecobaltpickaxe@gmail.com>
2021-05-26 13:42:48 -0300

    Added Drive Support

--- code.js
+++ code.js
@@ -1,27 +1,118 @@
-window.onload = function(){    
+var firebaseConfig = {
+    apiKey: "AIzaSyBk3-7opruOiHaYmcozSngRFhLF-SuxXJ0",
+    authDomain: "rocks-lasalle.firebaseapp.com",
+    databaseURL: "https://rocks-lasalle-default-rtdb.firebaseio.com",
+    projectId: "rocks-lasalle",
+    storageBucket: "rocks-lasalle.appspot.com",
+    messagingSenderId: "651642976112",
+    appId: "1:651642976112:web:2899e89412170614a6c1b5",
+    measurementId: "G-CTRNK734BE"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var user = undefined;
+var profile = undefined;
+var loginBtn = undefined;
+var accessToken = undefined;
+var picker = undefined;
+
+function onApiLoad(){
+    console.log("Google API loaded")
+    gapi.load("auth2", function(){
+
+    })
+    gapi.load("picker", function(){
+        console.log("Picker API loaded")
+    })
+}
+
+function onSignIn(googleUser){
+    //Get Info
+    user = googleUser
+    profile = googleUser.getBasicProfile()
+    loginBtn = document.getElementsByClassName("g-signin2")[0]
+    accessToken = googleUser.getAuthResponse(true).access_token;
+
+    //Create Logout
+    var newElement = document.createElement("img")
+    newElement.setAttribute("src", profile.getImageUrl())
+    newElement.setAttribute("style", "border-radius:50%; width:60px; height:60px;")
+    newElement.setAttribute("id", "loggedImage")
+    loginBtn.parentNode.replaceChild(newElement, loginBtn)
+    document.getElementById("logoutBtn").removeAttribute("hidden")
+
+    //Create Picker
+    var view = new google.picker.DocsView(google.picker.ViewId.DOCS_IMAGES)
+    view.setParent("1XT8sc_8BrFO4XsOTyrHoikj3a4CRIuGj")
+    picker = new google.picker.PickerBuilder()
+        .enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
+        .enableFeature(google.picker.Feature.NAV_HIDDEN)
+        .setAppId(651642976112)
+        .setOAuthToken(accessToken)
+        .addView(view)
+        .setDeveloperKey("AIzaSyBk3-7opruOiHaYmcozSngRFhLF-SuxXJ0")
+        .setCallback(pickImage)
+        .build();
+}
+
+function onSignOut(){
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.disconnect().then(function() {
+        var img = document.getElementById("loggedImage")
+        img.parentNode.replaceChild(loginBtn, img)
+        document.getElementById("logoutBtn").setAttribute("hidden", "")
+        profile = null
+        user = null
+    })
+}
+
+function writeEntry(rockName, rockType, desc, userEmail, imageID){
+    var rockRef = firebase.database().ref('rocks/' + rockName)
+    return rockRef.set({
+        owner: userEmail,
+        images: imageID,
+        type: rockType,
+        description: desc,
+    })
+}
+
+function pickImage(imgData){
+    if (imgData.action == "picked"){
+        const notLaSalle = document.getElementById("notLaSalleError")
+        const logIn = document.getElementById("logInError")
+        if (!profile){
+            notLaSalle.toggleAttribute("hidden", true)
+            logIn.removeAttribute("hidden")
+            return
+        }
+    
+        const email = profile.getEmail()
+        if (email.split("@")[1] != "soulasalle.com.br"){
+            notLaSalle.removeAttribute("hidden")
+            logIn.toggleAttribute("hidden", true)
+            return
+        }
+    
+        notLaSalle.toggleAttribute("hidden", true)
+        logIn.toggleAttribute("hidden", true)
+    
+        const data = Object.fromEntries(new FormData(form).entries())
+        images = []
+        imgData.docs.forEach(element => {
+            images.push(element.id)
+        });
+        writeEntry(data.rock, data.type, data.message, email, images).then(function(){
+            window.location.reload()
+        })
+    }
+}
+
+window.onload = function(){
     var form = document.getElementById("form")
     form.addEventListener("submit", (e) => {
         e.preventDefault()
-        const data = Object.fromEntries(new FormData(form).entries())
-        var desc = "Grupo: " + data.name + "\nPedra: " + data.rock + "\n\n" + data.message
-        var file = new Blob([desc], {type: "text/plaintext"})
-
-        const a = document.createElement('a');
-        a.href = URL.createObjectURL(file), "_blank"
-        a.download = data.rock + ".txt"
-        a.click()
-        a.remove()
+        picker.setVisible(true)
     }, false)
-
-    var whatsappBtn = document.getElementById("whatsapp")
-    whatsappBtn.addEventListener("click", (e) =>{
-        const data = Object.fromEntries(new FormData(form).entries())
-        var desc = "Grupo: " + data.name + "\nPedra: " + data.rock + "\n\n" + data.message
-        var file = new Blob([desc], {type: "text/plaintext"})
-
-        const a = document.createElement('a')
-        a.href = "https://api.whatsapp.com/send?phone=554598611989&text=" + encodeURIComponent(desc) +""
-        a.click()
-        a.remove()
-    })
 }
